8e5d7a3c6d5df6630801c40eb22c3301a6d7d0a0
Zygimantas <zygimantas.zilevicius@gmail.com>
2022-09-21 22:55:47 +0300

    added menu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,14 @@
 class ArticlesController < ApplicationController
 
     def show
-        # binding.break
         @article = Article.find(params[:id])
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
-        redirect_to articles_path
+        # rails 7 needs to set status
+        redirect_to articles_path, status: :see_other
     end
 
     def index
