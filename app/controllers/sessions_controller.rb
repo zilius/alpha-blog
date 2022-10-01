5e392af0e67441a75fd1e51e7d39fb866d468f9a
Zygimantas <zygimantas.zilevicius@gmail.com>
2022-10-01 12:45:10 +0300

    session con

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        # watafaka2s@one.lt
+        # qwerty
+    end
+
+    def destroy
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root to: "application#home"
   resources :articles
   get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  post "delete", to: "sessions#destroy"
   resources :users, except: [:new]
 end
