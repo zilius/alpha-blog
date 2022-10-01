233856da08ef4e12c5e9d08b216c46dfad12da40
Zygimantas <zygimantas.zilevicius@gmail.com>
2022-10-01 12:16:01 +0300

    pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,57 +1,54 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def show
+  end
 
-    def show
+  def destroy
+    @article.destroy
+    # rails 7 needs to set status
+    redirect_to articles_path, status: :see_other
+  end
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 2)
+  end
+
+  def new
+    @article = Article.new()
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render "edit"
     end
+  end
 
-    def destroy
-        @article.destroy
-        # rails 7 needs to set status
-        redirect_to articles_path, status: :see_other
+  def create
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+      flash[:notice] = "Created"
+      redirect_to @article
+      # redirect_to article_path(@article.id)
+    else
+      render "new"
     end
+  end
 
-    def index
-        @articles = Article.all()
-    end
+  private
 
-    def new
-        @article = Article.new()
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def edit
-    end
-
-    def update
-        if @article.update(article_params)
-            flash[:notice] = "Article was updated successfully"
-            redirect_to @article
-        else
-            render 'edit'
-        end
-    end
-
-    def create
-        @article = Article.new(article_params)
-        @article.user = User.first
-        if @article.save
-            flash[:notice] = "Created"
-            redirect_to @article
-            # redirect_to article_path(@article.id)
-        else
-            render 'new'
-        end
-    end
-
-    private 
-
-    def set_article
-        @article = Article.find(params[:id])
-    end
-
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
-
-
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 2)
   end
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
   def create
