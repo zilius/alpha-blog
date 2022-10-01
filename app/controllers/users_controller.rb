9a41acd8e61b1ba3840ae1079fa0a1afa54ab065
Zygimantas <zygimantas.zilevicius@gmail.com>
2022-10-01 14:54:53 +0300

    authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def new
     @user = User.new()
@@ -49,4 +51,12 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can edit only your account"
+      redirect_to @user
+    end
+  end
+
 end
